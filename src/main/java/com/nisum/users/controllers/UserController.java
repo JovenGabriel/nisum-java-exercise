6c21908120af8a2fcb283ccbee4cd2e26d91f66f
Gabriel <gabrielvenegas@comercialpau.cl>
2024-12-10 13:26:35 -0300

    Add OpenAPI annotations to UserController endpoints
    Enhance API documentation by including OpenAPI annotations for all endpoints in the UserController. This improves clarity by specifying endpoint summaries, descriptions, and potential response scenarios, including error handling for common HTTP status codes.

--- src/main/java/com/nisum/users/controllers/UserController.java
+++ src/main/java/com/nisum/users/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.nisum.users.dto.UserCreatedDTO;
 import com.nisum.users.dto.UserLoginDTO;
 import com.nisum.users.entities.User;
 import com.nisum.users.service.UserService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +30,8 @@ public class UserController {
      *
      * @return a ResponseEntity containing a list of User objects.
      */
+    @Operation(summary = "Get all users", description = "Retrieves a list of all available users")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of users")
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getUsers());
@@ -37,7 +43,11 @@ public class UserController {
      * @param id the unique identifier of the user to retrieve
      * @return a ResponseEntity containing the User object if found, or a suitable
      *         error response if the user does not exist
+     * @throws 404 Not Found if the user with the specified ID does not exist
      */
+    @Operation(summary = "Get a user by ID", description = "Retrieves a single user based on their unique ID")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the user")
+    @ApiResponse(responseCode = "404", description = "User with the specified ID not found", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"error\": \"User not found\"}")))
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getUserById(id));
@@ -48,7 +58,11 @@ public class UserController {
      *
      * @param userCreateDTO the data transfer object containing user creation details
      * @return a ResponseEntity containing the created user's details encapsulated in a UserCreatedDTO
+     * @throws 400 Bad Request if the input data is invalid
      */
+    @Operation(summary = "Create a new user", description = "Creates a new user based on provided details and returns their information")
+    @ApiResponse(responseCode = "201", description = "User successfully created")
+    @ApiResponse(responseCode = "400", description = "Invalid user input data", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"error\": \"Bad Request\" , \"fieldErrors\": { \"field\": \"error\"}}")))
     @PostMapping
     private ResponseEntity<UserCreatedDTO> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
         User user = userService.createUser(userCreateDTO);
@@ -67,7 +81,12 @@ public class UserController {
      *
      * @param userLoginDTO the DTO containing the user's email and password for authentication
      * @return a ResponseEntity containing the authenticated User
+     * @throws 400 Bad Request if the input login credentials are invalid
+     * @throws 404 Not Found if the user with email and password not matches
      */
+    @Operation(summary = "User login", description = "Authenticates a user based on their email and password")
+    @ApiResponse(responseCode = "200", description = "User successfully authenticated")
+    @ApiResponse(responseCode = "404", description = "Not Found if the user with email and password not matches", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"error\": \"Not found\" , \n \"message\": \"Invalid email or password\"}")))
     @PostMapping("/login")
     private ResponseEntity<User> loginUser(@Valid @RequestBody UserLoginDTO userLoginDTO) {
         return ResponseEntity.ok(userService.login(userLoginDTO));
